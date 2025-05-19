c1f40edd732cd78356792528b48f41e491b1b8e0
Santiago Sica <santisica29@gmail.com>
2025-05-19 00:10:41 -0300

    Create intial menu for the app

--- LibraryManagementSystem/Program.cs
+++ LibraryManagementSystem/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Spectre.Console;
+
+var menuChoices = new string[3] { "View Books", "Add Book", "Delete Book" };
+
+var choice = AnsiConsole.Prompt(
+    new SelectionPrompt<string>()
+    .Title("What do you want to do next?")
+    .AddChoices(menuChoices));
