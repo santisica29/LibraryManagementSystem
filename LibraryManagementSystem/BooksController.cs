a924a72eb7cbff293e9df93cee6bd6465b9020c9
Santiago Sica <santisica29@gmail.com>
2025-05-20 00:27:55 -0300

    create a class to keep book logic more organized

---
+++ LibraryManagementSystem/BooksController.cs
@@ -0,0 +1,69 @@
+﻿using Spectre.Console;
+
+namespace LibraryManagementSystem;
+internal static class BooksController
+{
+    private static List<string> books = new()
+    {
+        "The Great Gatsby", "To Kill a Mockingbird", "1984", "Pride and Prejudice", "The Catcher in the Rye", "The Hobbit", "Moby-Dick",
+        "War and Peace", "The Odyssey", "The Lord of the Rings", "Jane Eyre", "Animal Farm", "Brave New World", "The Chronicles of Narnia",
+        "The Diary of a Young Girl", "The Alchemist", "Wuthering Heights", "Fahrenheit 451", "Catch-22", "The Hitchhiker's Guide to the Galaxy"
+    };
+
+    internal static void ViewBooks()
+    {
+        AnsiConsole.MarkupLine("[yellow]List of Books:[/]");
+
+        foreach (var book in books)
+        {
+            AnsiConsole.MarkupLine($"- [cyan]{book}[/]");
+        }
+
+        AnsiConsole.MarkupLine("Press Any Key to Continue.");
+        Console.ReadKey();
+    }
+    internal static void AddBook()
+    {
+        var title = AnsiConsole.Ask<string>("Enter the [green]title[/] of the book to add:");
+
+        if (books.Contains(title))
+        {
+            AnsiConsole.MarkupLine("[red]This book already exists.[/]");
+        }
+        else
+        {
+            books.Add(title);
+            AnsiConsole.MarkupLine("[green]Book added successfully![/]");
+        }
+
+        AnsiConsole.MarkupLine("Press Any Key to Continue.");
+        Console.ReadKey();
+    }
+
+    internal static void DeleteBook()
+    {
+        if (books.Count == 0)
+        {
+            AnsiConsole.MarkupLine("[red]No books available to delete.[/]");
+            Console.ReadKey();
+            return;
+        }
+
+        var bookToDelete = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+            .Title("Select a [red]book[/] to delete:")
+            .AddChoices(books));
+
+        if (books.Remove(bookToDelete))
+        {
+            AnsiConsole.MarkupLine("[red]Book deleted successfully![/]");
+        }
+        else
+        {
+            AnsiConsole.MarkupLine("[red]Book not found.[/]");
+        }
+
+        AnsiConsole.MarkupLine("Press any key to continue.");
+        Console.ReadKey();
+    }
+}
--- LibraryManagementSystem/Program.cs
+++ LibraryManagementSystem/Program.cs
@@ -1,13 +1,9 @@
-﻿using Spectre.Console;
+﻿using LibraryManagementSystem;
+using Spectre.Console;
 
 var menuChoices = new MenuOption[3] { MenuOption.ViewBooks, MenuOption.AddBook, MenuOption.DeleteBook };
 
-var books = new List<string>()
-{
-    "The Great Gatsby", "To Kill a Mockingbird", "1984", "Pride and Prejudice", "The Catcher in the Rye", "The Hobbit", "Moby-Dick",
-    "War and Peace", "The Odyssey", "The Lord of the Rings", "Jane Eyre", "Animal Farm", "Brave New World", "The Chronicles of Narnia",
-    "The Diary of a Young Girl", "The Alchemist", "Wuthering Heights", "Fahrenheit 451", "Catch-22", "The Hitchhiker's Guide to the Galaxy"
-};
+
 while (true)
 {
     Console.Clear();
@@ -20,71 +16,15 @@ while (true)
     switch (choice)
     {
         case MenuOption.ViewBooks:
-            ViewBooks();
+            BooksController.ViewBooks();
             break;
         case MenuOption.AddBook:
-            AddBook();
+            BooksController.AddBook();
             break;
         case MenuOption.DeleteBook:
-            DeleteBook();
+            BooksController.DeleteBook();
             break;
     }
-}
-void ViewBooks()
-{
-    AnsiConsole.MarkupLine("[yellow]List of Books:[/]");
-
-    foreach (var book in books)
-    {
-        AnsiConsole.MarkupLine($"- [cyan]{book}[/]");
-    }
-
-    AnsiConsole.MarkupLine("Press Any Key to Continue.");
-    Console.ReadKey();
-}
-void AddBook()
-{
-    var title = AnsiConsole.Ask<string>("Enter the [green]title[/] of the book to add:");
-
-    if (books.Contains(title))
-    {
-        AnsiConsole.MarkupLine("[red]This book already exists.[/]");
-    }
-    else
-    {
-        books.Add(title);
-        AnsiConsole.MarkupLine("[green]Book added successfully![/]");
-    }
-
-    AnsiConsole.MarkupLine("Press Any Key to Continue.");
-    Console.ReadKey();
-}
-
-void DeleteBook()
-{
-    if (books.Count == 0)
-    {
-        AnsiConsole.MarkupLine("[red]No books available to delete.[/]");
-        Console.ReadKey();
-        return;
-    }
-
-    var bookToDelete = AnsiConsole.Prompt(
-        new SelectionPrompt<string>()
-        .Title("Select a [red]book[/] to delete:")
-        .AddChoices(books));
-
-    if (books.Remove(bookToDelete))
-    {
-        AnsiConsole.MarkupLine("[red]Book deleted successfully![/]");
-    }
-    else
-    {
-        AnsiConsole.MarkupLine("[red]Book not found.[/]");
-    }
-
-    AnsiConsole.MarkupLine("Press any key to continue.");
-    Console.ReadKey();
 }
 
 enum MenuOption
