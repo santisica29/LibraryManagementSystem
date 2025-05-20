458f1db8780539056d4d259f2cd7ddc7c72bfdcf
Santiago Sica <santisica29@gmail.com>
2025-05-20 11:04:30 -0300

    learn about constructors

---
+++ LibraryManagementSystem/Book.cs
@@ -0,0 +1,25 @@
+﻿namespace LibraryManagementSystem;
+internal class Book
+{
+    string Name = "Unknown";
+    int Pages = 0;
+
+    internal Book()
+    {
+        Name = "Unkown";
+        Pages = 0;
+    }
+    internal Book(int pages)
+    {
+        Name = "Unknown";
+        Pages = pages;
+        Console.WriteLine($"Name: {Name}, Pages = {Pages}");
+    }
+
+    internal Book(string name, int pages)
+    {
+        Name = name;
+        Pages = pages;
+        Console.WriteLine($"Name: {Name}, Pages = {Pages}");
+    }
+}
--- LibraryManagementSystem/Program.cs
+++ LibraryManagementSystem/Program.cs
@@ -3,8 +3,9 @@
 //UserInterface userInterface = new();
 //userInterface.MainMenu();
 
-(string Title, int Pages) book1 = ("Frankenstein", 350);
-Console.WriteLine($"Title: {book1.Title}, Pages: {book1.Pages}");
+var book1 = new Book("Hamlet", 200);
+var book2 = new Book("Great Gatsby", 300);
+var book3 = new Book();
 
 
 
