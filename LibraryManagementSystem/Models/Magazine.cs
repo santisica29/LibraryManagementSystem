37894b5de37b4860b1e29fcdf74b124893b87e7a
Santiago Sica <santisica29@gmail.com>
2025-05-22 12:28:43 -0300

    create magazine class that inherits from libraryItem

---
+++ LibraryManagementSystem/Models/Magazine.cs
@@ -0,0 +1,36 @@
+﻿using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Models
+{
+    internal class Magazine : LibraryItem
+    {
+        public string Publisher { get; set; }
+        public DateTime PublishDate { get; set; }
+        public int IssueNumber { get; set; }
+
+        public Magazine (int id, string name, string publisher, DateTime publishDate, string location, int issueNumber)
+            : base (id, name, location)
+        {
+            Publisher = publisher;
+            PublishDate = publishDate;
+            IssueNumber = issueNumber;
+        }
+        public override void DisplayDetails()
+        {
+            var panel = new Panel(new Markup($"[bold]Magazine:[/] [cyan]{Name}[/] published by [cyan]{Publisher}[/]") +
+                                  $"\n[bold]Publish Date:[/] {PublishDate:yyyy-MM-dd}" +
+                                  $"\n[bold]Issue Number:[/] {IssueNumber}" +
+                                  $"\n[bold]Location:[/] [blue]{Location}[/]")
+            {
+                Border = BoxBorder.Rounded
+            };
+
+            AnsiConsole.Write(panel);
+        }
+    }
+}
